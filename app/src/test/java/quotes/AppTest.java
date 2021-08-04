76698e5c9ece3e68337ec37324e9ca385e15748e
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-08-04 19:44:50 +0300

    finish lab08

--- app/src/test/java/quotes/AppTest.java
+++ app/src/test/java/quotes/AppTest.java
@@ -7,8 +7,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    /////////////////////////////////////////// LAB 08 ///////////////////////////////////////////
+    // empty file test
+    @Test void testGetOneQuote() {
+        String path = "E:\\Coding\\LTUC Course\\401\\quotes\\resources\\empty.json";
+        String out = "json is empty";
+
+        assertEquals(out,MyMethods.getOneQuote(path),"test get quotes from json file");
+    }
+
+    // happy path test
+    @Test void testGetOneQuote2() {
+        String path = "E:\\Coding\\LTUC Course\\401\\quotes\\resources\\test.json";
+        String out = "Ask no questions, and you'll be told no lies";
+
+        assertEquals(out,MyMethods.getOneQuote(path),"test get quotes from json file");
     }
 }
